0f5806e9e0277b867220ff1ed0e8e8692bf92e73
Antonioxam <antoniojesus.ibanez.lopez@ces-vegamedia.es>
2024-04-18 01:39:50 +0200

    carpeta

--- ejemplo2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 1 ] ; then
-echo "Bien un parametro"
-else
-echo "Es mas de un parametro"
-fi
--- ejemplo5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enter a word:"
-read word
-reverse=$(echo $word | rev)
-if [ $word = $reverse ]; then
-  echo "The word is a palindrome"
-else
-  echo "The word is not a palindrome"
-fi
